6a0d23e69bed66d179b1af911cc215bb7afc0687
cseitz5 <cseitz5@kent.edu>
2020-10-25 19:42:55 -0400

    ok pls pass

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => res.send('bruuuuuuh u got haaaacccckkeeed'));
+app.get('/', (req, res) => res.send('welcome to the root of my website'));
 
 let port = 8080;
 if (process.env.PORT) {

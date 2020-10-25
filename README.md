ac40fb9d3e32f0106b4c46a31dbaf570a702a75c
cseitz5 <cseitz5@kent.edu>
2020-10-25 18:48:17 -0400

    first commit ok

---
+++ README.md
@@ -0,0 +1,9 @@
+Setting up Travis CI and continuous deployment
+
+
+1. Set up your project. duh
+2. Sign up and approve Travis CI
+3. Add .travis.yml file
+```
+
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => res.send('hi there'));
+
+let port = 8080;
+if (process.env.PORT) {
+    port = process.env.PORT;
+}
+app.listen(port);
